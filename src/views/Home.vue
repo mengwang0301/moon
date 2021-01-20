5691be0bcf998b6509b0726b1a4712f740ce746f
王萌 <warm0301@sina.com>
2021-01-20 23:42:34 +0800

    根据顶点画面, 解决缝隙问题

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,17 +42,19 @@
 
         let ambient = new THREE.AmbientLight(0xcccccc)
         this.scene.add(ambient)
+        // let directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+        // this.scene.add( directionalLight );
 
-        // let axes = new THREE.AxesHelper(100000)
-        // this.scene.add(axes)
+        let axes = new THREE.AxesHelper(100000)
+        this.scene.add(axes)
 
         let container = document.getElementById('container')
         let width = container.clientWidth// 窗口宽度
         let height = container.clientHeight// 窗口高度
         // 创建相机对象
         this.camera = new THREE.PerspectiveCamera(30, width / height, 1, 50000)
-        this.camera.position.set(0, 0, 2000)// 设置相机位置
-        this.camera.lookAt(new THREE.Vector3(size * column / 2, 0, size * row / 2))// 设置相机方向(指向的场景对象)
+        this.camera.position.set(2000, 2000, 2000)// 设置相机位置
+        this.camera.lookAt(0,0,0)// 设置相机方向(指向的场景对象)
 
         /**
          * 创建渲染器对象
@@ -77,20 +79,50 @@
         land.position.x = size * column / 2
         land.position.z = size * row / 2
         land.rotation.x = -Math.PI / 2
-        // this.scene.add(land)
+        let vertices = []
         for (let i = 0; i < row; i++) {
           for (let j = 0; j < column; j++) {
-            let mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(size+1, size+1), groundMaterial)
             const height = data[column * i + j]
-            mesh.position.x = (size * j)
-            mesh.position.y = height - 2200
-            mesh.position.z = (size * i)
-            mesh.rotation.x = -Math.PI / 2
-            // mesh.receiveShadow = true
-            // console.log('x= '+ size*j+'   y = ' + size*i )
-            this.scene.add(mesh)
+            vertices.push(new THREE.Vector3(size * j, height - 2200, size * i))
           }
         }
+
+        // 立方体
+        let cubeGeometry = new THREE.Geometry()
+        cubeGeometry.vertices = vertices
+
+        let faces=[]
+        for (let i = 0; i < row - 1; i++) {
+          for (let j = 0; j < column - 1; j++) {
+            let face1 = new THREE.Face3(row*i+j, row*(i+1)+j, row*i + j + 1)
+            const y1 = vertices[face1.a].y
+            const y2 = vertices[face1.b].y
+            const y3 = vertices[face1.c].y
+            face1.vertexColors = [
+              new THREE.Color(y1 - Math.floor(y1), y1 - Math.floor(y1), y1 - Math.floor(y1)),
+              new THREE.Color(y2 - Math.floor(y2), y2 - Math.floor(y2), y2 - Math.floor(y2)),
+              new THREE.Color(y3 - Math.floor(y3), y3 - Math.floor(y3), y3 - Math.floor(y3)),
+            ]
+
+            let face2 = new THREE.Face3(row*i + j + 1, row*(i+1)+j, row*(i+1)+j+1)
+            const y4 = vertices[face2.a].y
+            const y5 = vertices[face2.b].y
+            const y6 = vertices[face2.c].y
+            face2.vertexColors = [
+              new THREE.Color(y4 - Math.floor(y4), y4 - Math.floor(y4), y4 - Math.floor(y4)),
+              new THREE.Color(y5 - Math.floor(y5), y5 - Math.floor(y5), y5 - Math.floor(y5)),
+              new THREE.Color(y6 - Math.floor(y6), y6 - Math.floor(y6), y6 - Math.floor(y6)),
+            ];
+            faces.push(face1)
+            faces.push(face2)
+          }
+        }
+        cubeGeometry.faces = faces;
+        const cube = new THREE.Mesh(cubeGeometry,new THREE.MeshBasicMaterial({
+          vertexColors : THREE.VertexColors,
+          side: THREE.DoubleSide
+        }));
+        this.scene.add(cube);
       }
     }
   }
