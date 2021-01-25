7847121e9e7755454683ee7211202da5b0b4e995
王萌 <wangmeng@siasun.com>
2021-01-25 16:37:11 +0800

    commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -18,6 +18,7 @@
         earthworm: null,
         xOffset: 0,
         zOffset: 0,
+        earthwormPosArr: []
       }
     },
     mounted() {
@@ -31,6 +32,9 @@
         this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 100000);
         this.camera.position.set(280, 130, 0);		//相机位置
         this.scene = new THREE.Scene();
+
+        let axes = new THREE.AxesHelper(100000)
+        this.scene.add(axes)
 
         this.renderer = new THREE.WebGLRenderer({antialias: true, alpha: true})
         this.renderer.setSize(window.innerWidth, window.innerHeight)
@@ -66,23 +70,46 @@
         // let earthworm
 
         loader.load(`${process.env.BASE_URL}models/jiantou.gltf`, gltf => {
+          // let children = []
+          // const group = new THREE.Group()
+          // for (let i = 0; i < 5; i++) {
+          //   const child = gltf.scene.children[0];
+          //   child.position.set((5-i)*10, 0, 0)
+          //   this.earthwormPosArr.push(child.position)
+          //   // children.push(child)
+          //   group.add(child)
+          // }
           this.earthworm = gltf.scene;
-          console.log(gltf.animations)
+          // this.earthworm = gltf.scene.children[0];
+
+          // group.children = children
+          // console.log(group)
+          // this.earthworm = group
+          // console.log(gltf.scene)
+
           this.earthworm.scale.set(1000, 1000, 1000);
           this.earthworm.rotation.set(Math.PI, 0, 0)
-          this.earthworm.position.set(15, 0, 0);
+          // this.earthworm.position.set(15, 0, 0);
+
+
+
+
+
           this.earthworm.traverse(child => {
+            // this.earthwormPosArr.push(child.position)
             if (child.isMesh) {
               child.material.envMap = textureCube;
               child.material.envMapIntensity = 2;
               child.material.side = THREE.DoubleSide;
             }
           });
-          console.log(this.earthworm)
+          // console.log(this.earthworm)
           this.scene.add(this.earthworm);
         }, undefined, err=> {
-          console.log(err)
+          console.error(err)
         });
+
+
 
         loader.load(`${process.env.BASE_URL}models/gltf/fangzi.gltf`, gltf => {
           thismodel1 = gltf.scene;
@@ -157,11 +184,28 @@
 
       updateQiuyin() {
         if (this.earthworm) {
-          this.earthworm.position.x += this.xOffset
-          this.earthworm.position.z += this.zOffset
+          // this.earthworm.position.x += this.xOffset
+          // this.earthworm.position.z += this.zOffset
           // console.log(this.xOffset)
           // this.earthworm.position.x += this.xOffset
           // this.earthworm.position.z += this.zOffset
+          // // 先取出数组中第一个元素
+          // if (this.xOffset ===0 && this.zOffset === 0) {
+          //   return
+          // }
+          const pos = new THREE.Vector3(this.earthwormPosArr[0].x += this.xOffset/1000, this.earthwormPosArr[0].y, this.earthwormPosArr[0].z += this.zOffset/1000)
+
+          this.earthwormPosArr.splice(0,0, pos)
+          // console.log(this.earthwormPosArr.length)
+          this.earthwormPosArr.splice(this.earthwormPosArr.length-1,1)
+          // console.log(this.earthwormPosArr.length)
+          // console.log('----------')
+          // console.log(this.earthwormPosArr)
+          this.earthwormPosArr.forEach((v,i)=>{
+            const obj = this.earthworm.children[i]
+            // obj.scale.set(1000, 1000, 1000);
+            obj.position = v
+          })
         }
       },
 
